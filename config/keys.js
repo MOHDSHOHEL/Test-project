aba3e13d11847aa0a37ad99fc3a1dec90e37be4a
MOHDSHOHEL <shohelsayyed123@gmail.com>
2023-02-22 16:53:06 +0530

    files added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//This is mongo URI link,database is created in Atlas
+
+module.exports={
+    MongoURI:'mongodb+srv://shohel:password%40123@cluster0.svnxmlh.mongodb.net/?retryWrites=true&w=majority'
+}
